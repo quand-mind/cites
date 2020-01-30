14c68ade862229703b6e3f1e79c0fb52eb224651
0x6A6F736564616E3130 <josedan10@users.noreply.github.com>
2020-01-30 12:05:12 -0400

    Fixed jQuery bug for chatter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
+
+    {{-- jQuery for chatter --}}
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="app">

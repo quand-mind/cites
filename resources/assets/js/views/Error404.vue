ed1639796e161ff7639cae55268c703e83fdc2f8
severalreves <gabrielalexander2@gmail.com>
2020-02-01 11:43:40 -0400

    Error 404 listo

---
+++ resources/assets/js/views/Error404.vue
@@ -0,0 +1,18 @@
+<!-- 
+Ultima Actualización: 16/01/2020
+
+Tipo de componente: Vista
+
+Ruta:/somos/grupos
+
+Descripción:
+	Descripción de los distintos grupos
+ -->
+
+<template>
+	<div class="d-flex flex-column align-items-center">
+		<h1 class="w-75 text-black-50 display-2"><span class="icon icon-cross"> ERROR 404 </span></h1>
+		<h3 class="w-75 w-lg-50 w-xl-50 text-black-50 mt-5">Lo sentimos la página que estás buscando no existe. Te invitamos a seguir navegando en <router-link to="/"><span class="icon icon-home"></span> Minec - Home</router-link></h3>
+
+    </div>
+</template>

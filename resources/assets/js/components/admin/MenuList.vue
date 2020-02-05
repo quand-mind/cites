759405bfa5ed50275a8ebfcd849967d0c7ab578e
quand-mind <quand.mind@gmail.com>
2020-02-04 22:54:58 -0430

    add page menu drag&drop

--- resources/assets/js/components/admin/MenuList.vue
+++ resources/assets/js/components/admin/MenuList.vue
@@ -3,9 +3,14 @@
         <draggable v-model="pagesList">
             <transition-group type="transition" name="flip-list">
                 <div v-for="element in pagesList" :key="element.id">
-                    b-
-                    <span>{{element.title}}</span>
-                    <span>{{element.id}}</span>
+                    <b-button v-b-toggle.collapse-1 variant="primary">
+                        <span>{{element.title}}</span>
+                    </b-button>
+                    <b-collapse id="collapse-1" class="mt-2">
+                        <b-card>
+                            <span>{{element.id}}</span>
+                        </b-card>
+                    </b-collapse>
                 </div>
             </transition-group>
         </draggable>

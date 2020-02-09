fd265d9e99b026e147ef66cb3892d1f2aba52e8b
severalreves <gabrielalexander2@gmail.com>
2020-02-09 05:16:39 -0400

    homeDashboard con iconos

--- resources/assets/js/components/admin/HomeDashboard.vue
+++ resources/assets/js/components/admin/HomeDashboard.vue
@@ -11,8 +11,8 @@
         </template>
         <b-card-text>Revisa una de las páginas o modifica una creada</b-card-text>
         <b-button-group>
-            <b-button href="/dashboard/pages/create" variant="primary" class="mx-2">Crear una nueva página</b-button>
-            <b-button href="/dashboard/pages" variant="info" class="mx-2">Administrar páginas</b-button>
+            <b-button href="/dashboard/pages/create" variant="primary" class="mx-2"><font-awesome-icon :icon="['fas', 'plus']"></font-awesome-icon><br> Crear una nueva página</b-button>
+            <b-button href="/dashboard/pages" variant="info" class="mx-2"><font-awesome-icon :icon="['fas', 'book-open']"></font-awesome-icon><br> Administrar páginas</b-button>
         </b-button-group>
       </b-card>
 
@@ -21,7 +21,7 @@
           <h6 class="mb-0 text-uppercase">Menú</h6>
         </template>
         <b-card-text>Modifica el orden o los botones del menú</b-card-text>
-        <b-button href="/dashboard/menu" variant="primary"class="mx-2">Modificar menú</b-button>
+        <b-button href="/dashboard/menu" variant="primary"class="mx-2"><font-awesome-icon :icon="['fas', 'pager']"></font-awesome-icon><br> Modificar menú</b-button>
       </b-card>
 
       <b-card header-tag="header" class="m-3">
@@ -30,9 +30,9 @@
         </template>
         <b-card-text>Ten el control de todas los usuarios que administran el sitio y de los roles que tendrán</b-card-text>
           <b-button-group>
-            <b-button href="/dashboard/users" variant="primary" class="mx-2">Crear usuarios</b-button>
-            <b-button href="/dashboard/users" variant="info" class="mx-2">Modificar usuarios</b-button>
-            <b-button href="/dashboard/users" variant="danger" class="mx-2">Eliminar usuarios</b-button>
+            <b-button href="/dashboard/users" variant="primary" class="mx-2"><font-awesome-icon :icon="['fas', 'user-plus']"></font-awesome-icon><br> Crear usuarios</b-button>
+            <b-button href="/dashboard/users" variant="info" class="mx-2"><font-awesome-icon :icon="['fas', 'users-cog']"></font-awesome-icon><br> Modificar usuarios</b-button>
+            <b-button href="/dashboard/users" variant="danger" class="mx-2"><font-awesome-icon :icon="['fas', 'user-times']"></font-awesome-icon><br> Eliminar usuarios</b-button>
           </b-button-group>
       </b-card>
     </b-card-group>
@@ -42,9 +42,9 @@
           <h6 class="mb-0 text-uppercase">Preguntas Frecuentes</h6>
         </template>
         <b-button-group>
-            <b-button href="/dashboard/questions" variant="primary" class="mx-2">Crear nueva pregunta</b-button>
-            <b-button href="/dashboard/questions" variant="info" class="mx-2">Responder preguntas</b-button>
-            <b-button href="/dashboard/questions" variant="danger" class="mx-2">Administrar preguntas</b-button>
+            <b-button href="/dashboard/questions" variant="primary" class="mx-2"><font-awesome-icon :icon="['fas', 'plus']"></font-awesome-icon><br> Crear nueva pregunta</b-button>
+            <b-button href="/dashboard/questions" variant="info" class="mx-2"><font-awesome-icon :icon="['fas', 'question-circle']"></font-awesome-icon><br> Responder preguntas</b-button>
+            <b-button href="/dashboard/questions" variant="danger" class="mx-2"><font-awesome-icon :icon="['fas', 'cog']"></font-awesome-icon><br> Administrar preguntas</b-button>
           </b-button-group>
       </b-card>
        <b-card header-tag="header">
@@ -52,8 +52,8 @@
           <h6 class="mb-0 text-uppercase">Encuestas</h6>
         </template>
         <b-button-group>
-            <b-button href="/dashboard/surveys" variant="primary" class="mx-2">Crear nueva encuesta</b-button>
-            <b-button href="/dashboard/surveys" variant="info" class="mx-2">Administrar encuestas</b-button>
+            <b-button href="/dashboard/surveys" variant="primary" class="mx-2"><font-awesome-icon :icon="['fas', 'plus']"></font-awesome-icon><br> Crear nueva encuesta</b-button>
+            <b-button href="/dashboard/surveys" variant="info" class="mx-2"><font-awesome-icon :icon="['fas', 'cog']"></font-awesome-icon><br> Administrar encuestas</b-button>
           </b-button-group>
       </b-card>
     </b-card-group>

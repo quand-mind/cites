bff4c7d73ad01f37722226507989b5d3d0d2c8b1
alejandro27647120 <jasve504@gmail.com>
2021-07-27 13:47:00 -0400

    [add]: create OfficialsTableSeeder

---
+++ database/seeders/OfficialsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\officials;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class OfficialsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('officials')->insert([
+            //'name'      => 'Admin',
+            //'dni'       => '27647120'
+            'email'     => 'admin@mail.com',
+            'password'  => bcrypt('admin123'),
+            'username'  => 'admin',
+            'role'      => 'admin',
+            'user_id'   => 1,
+            'remember_token' => Str::random(10)
+        ]);
+        DB::table('officials')->insert([
+            //'name'      => 'Admin',
+            //'dni'       => '27647120'
+            'email'     => 'superuser@mail.com',
+            'password'  => bcrypt('super123'),
+            'username'  => 'superuser',
+            'role'      => 'superuser',
+            'user_id'   => 2,
+            'remember_token' => Str::random(10)
+        ]);
+        DB::table('officials')->insert([
+            //'name'      => 'Admin',
+            //'dni'       => '27647120'
+            'email'     => 'writer@mail.com',
+            'password'  => bcrypt('writer123'),
+            'username'  => 'writertester',
+            'role'      => 'writer',
+            'user_id'   => 3,
+            'remember_token' => Str::random(10)
+        ]);
+    }
+}

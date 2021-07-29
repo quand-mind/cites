af18f2b0ccefee217d8a1f1a952b0149d14a91c9
Jesús Acevedo <jasve504@gmail.com>
2021-07-28 20:21:34 -0400

    [fix]: add method in model client

--- app/Models/client.php
+++ app/Models/client.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\client as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use Tymon\JWTAuth\Contracts\JWTSubject;
 

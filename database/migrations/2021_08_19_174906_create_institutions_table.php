5c82025b2b84abd332fcdbaf5ea279e5c32c69cb
alejandro27647120 <jasve504@gmail.com>
2021-08-19 14:21:56 -0400

    [add]: add model Institucions

---
+++ database/migrations/2021_08_19_174906_create_institutions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstitutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->foreignId('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('institutions');
+    }
+}

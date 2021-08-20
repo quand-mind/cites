a4116bcc15ccb92bae8de4ca6f915f6f3c9359e1
Jesús Acevedo <jasve504@gmail.com>
2021-08-20 10:18:21 -0400

    [fix]: add user authenticate in finction storeInstitution

--- app/Http/Controllers/InstitutionController.php
+++ app/Http/Controllers/InstitutionController.php
@@ -10,16 +10,16 @@ class InstitutionController extends Controller
 {
     public function storeInstitution(Request $request){
 
-        /*if($request->validate([
+        if($request->validate([
             'name' => 'required',
             'institutional_email' => 'required',
         ])){
-            try {*/
+            try {
                 
                 $institution = new Institution();
                 $institution->name = $request->input('name');
-                $institution->institutional_email = $request->input('email');
-                $institution->client_id = 1;
+                $institution->institutional_email = $request->input('institutional_email');
+                $institution->client_id = auth()->user()->id;
                 $institution->save();
 
                 $phones= $request->only('phones');
@@ -30,11 +30,11 @@ class InstitutionController extends Controller
                 }, $phones);
                 $institution->phones()->createMany($phones);
                 return "Las institucion se ha agregado correctamente.";
-           /* } catch (\Exception $err) {
+           } catch (\Exception $err) {
                 return response(json_encode($err), 500);
-            }*/
-       /* }else {
+            }
+       }else {
             return response('Error en validación');
-        }*/
+        }
     }
 }

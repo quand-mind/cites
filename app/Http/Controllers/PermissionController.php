2ab4e742decd09d7a837633d6b581689d82a7e6e
Jose Quintero <josedan10>
2021-10-02 02:31:47 -0400

    fix: fixed QR route problem

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -140,7 +140,7 @@ class PermissionController extends Controller
         $image = base64_encode(file_get_contents(public_path('/images/logos/logo-minec.png')));
         $logo = $image;
         $host = $_SERVER["HTTP_HOST"];
-        $GcodeQr = QrCode::generate($host.'/permitInfo/'.$permit->request_permit_no, '../public/qrcodes/'.$permit->request_permit_no.'.svg');
+        $GcodeQr = QrCode::generate($host.'/permitInfo/'.$permit->request_permit_no, storage_path('app/files/qrcodes/'.$permit->request_permit_no.'.svg'));
         $codeQr = base64_encode(file_get_contents(storage_path('app/files/qrcodes/'.$permit->request_permit_no.'.svg')));
         $pdf->loadView('permissions.aproved_permit', [ 'permit' => $permit, 'logo' => $image, 'codeQr' => $codeQr]);
         return $pdf->stream();

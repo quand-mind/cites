92f581a7437e505b0328bb9df93c5a0a48a447a1
jesus-ace <jasve504@gmail.com>
2021-10-11 19:11:52 -0400

    return data fort chart in view

--- resources/views/species.blade.php
+++ resources/views/species.blade.php
@@ -2,111 +2,56 @@
 
 @section('content')
     <h1>Consuming the Species + / CITE api</h1>
-    {{--<!--div class="row p-4 w-100">
-        <div class="col-12 d-flex flex-wrap justify-content-center p-4">
-        <div class="col-5 p-2">
-                <label for="">Appendix</label>
-                <select class="custom-select" id="inputGroupSelect01">
-                    @foreach($species->taxon_concepts as $specie)
-                        <option selected>{{ $specie->cites_listing }}
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Kingdom</label>
-                <select class="custom-select" id="inputGroupSelect01">    
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "KINGDOM")
-                            <option selected>{{$specie->full_name}}<option>
-                            <option selected>{{$species->taxon_concepts[45]->full_name}}</option>
-                        @endif
-                    @endforeach
-                    
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Phylum</label>
-                <select class="custom-select" id="inputGroupSelect01" >
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "PHYLUM")
-                            <option selected>{{$specie->higher_taxa->phylum}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Class</label>
-                <select class="custom-select" >
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "CLASS")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Order</label>
-                <select class="custom-select" id="inputGroupSelect01" >
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "ORDER")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Family</label>
-                <select class="custom-select" id="inputGroupSelect01">
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "FAMILY")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Genus</label>
-                <select class="custom-select" id="inputGroupSelect01">
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "GENUS")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Species</label>
-                <select class="custom-select" id="inputGroupSelect01">
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "SPECIES")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-5 p-2">
-                <label for="">Subspecies</label>
-                <select class="custom-select" id="inputGroupSelect01">
-                    @foreach($species->taxon_concepts as $specie)
-                        @if($specie->rank == "SUBSPECIES")
-                            <option selected>{{$specie->full_name}}
-                        @endif
-                    @endforeach
-                </select>
+    
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body">
+                        <canvas id="canvas" height="280" width="600"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
-    </div-->--}}
+    </div>    
 
-    <form class="form-inline">
-        <input name="filter" class="form-control mr-sm-2" type="search" placeholder="Buscar por nombre" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-    </form>
-    <div>
-        @foreach($especies as $specie)
-            
-                {{$specie->full_name}} {{$specie->author_year}}
-            
-        @endforeach
-       
-    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+
+    <script>
+
+        var status = <?php echo $status; ?>;
+        var user = <?php echo $statusCount; ?>;
+        var barChartData = {
+            labels: year,
+            datasets: [{
+                label: 'User',
+                backgroundColor: "pink",
+                data: user
+            }]
+        };
+        window.onload = function() {
+            var ctx = document.getElementById("canvas").getContext("2d");
+            window.myBar = new Chart(ctx, {
+                type: 'bar',
+                data: barChartData,
+                options: {
+                    elements: {
+                        rectangle: {
+                            borderWidth: 2
+                            borderColor: '#c1c1c1',
+                            borderSkipped: 'bottom'
+                        }
+                    },
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'total de permisos por status'
+    
+                    }
+                }
+            });
+        };
+    </script>    
 @endsection

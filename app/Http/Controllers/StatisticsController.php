8c6b5fc532e0391c88ec83b6dcad009d6cdf87e3
Jesús Acevedo <jasve504@gmail.com>
2021-10-12 12:07:37 -0400

    fix query for generate charts statistics

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -14,11 +14,19 @@ use App\Http\Controllers\ApiController;
 class StatisticsController extends Controller
 {
     public function TestExportExcel(){
-       //$permitStatus = Permit::get();
-        $labels =["requested", "committed", "valid" ];
-
+        $permitStatus = Permit::get();
+        $prelabels = [];
+        foreach ($permitStatus as $permitStatu) {
+            $prelabels[]= $permitStatu->status;
+        }
+        $secondlabels = array_unique($prelabels);
+        
+        $labels = [];
         $data = [];
-        foreach ($labels  as $label) {
+        foreach ($secondlabels  as $secondlabel) {
+            $labels[]=$secondlabel;
+        }
+        foreach ($labels as $label) {
             $data[] = Permit::where( 'status', '=', $label)->count();
         }
         return view('species')->with('labels',json_encode($labels))->with('data',json_encode($data,JSON_NUMERIC_CHECK));
@@ -32,33 +40,40 @@ class StatisticsController extends Controller
         foreach ($countries as $countrie) {
             $arrayCountries[] = $countrie->label;
         }
-        //return $arrayCountries;
-        $labels=[];
+        
+        $prelabels=[];
         $data= [];
         foreach ($arrayCountries as $arrayCountry) {
             $verifyCountry = Permit::where('country', '=', $arrayCountry)->get();
             if (count($verifyCountry)) {
                 foreach ($verifyCountry as $verifyCountrys) {
-                    $labels[]= $verifyCountrys->country;
-                    $data[] = Permit::where( 'country', '=', $verifyCountrys->country)->count();
+                    $prelabels[]= $verifyCountrys->country;
                 }
             }
+        }
+        return $labels = array_unique($prelabels);
+        foreach ($labels as $label) {
+            $data[] = Permit::where( 'country', '=', $label)->count();
         }
         return view('species')->with('labels',json_encode($labels))->with('data',json_encode($data,JSON_NUMERIC_CHECK));
     }
 
     public function chartForpermitType(){
         $permitTypes = Permit::join('permit_types', 'permit_types.id', '=', 'permit_type_id')->get();
-        $labels=[];
+        $prelabels=[];
         $data= [];
         foreach ($permitTypes as $permitType) {
             $verifypermitType = Permit::join('permit_types', 'permit_types.id', '=', 'permit_type_id')->where('name', '=', $permitType->name)->get();
             if (count($verifypermitType)) {
-                $labels[]= $permitType->name;
+                $prelabels[]= $permitType->name;
             }
 
-        }
-        return $labels;
+        };
+        $labels = array_unique($prelabels);
+        foreach ($labels as $label) {
+            $data[]= Permit::join('permit_types', 'permit_types.id', '=', 'permit_type_id')->where('name', '=', $label)->count();
+        };
+        return view('species')->with('labels',json_encode($labels))->with('data',json_encode($data,JSON_NUMERIC_CHECK));
     }
     
 }
